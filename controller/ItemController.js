10929b836878b26cbaa2038ea06c08d9658423ee
danukarangith <danukarangith456@gmail.com>
2024-08-09 14:05:13 +0530

    item implemnt

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,246 +1,278 @@
-getAllItem();
-
-// --------------Save btn event---------------------------
-$("#btnSaveItem").click(function () {
-  if (checkAllItems()) {
-    saveItem();
-  } else {
-    alert("Something went wrong!");
-  }
+//update the items
+$('#btnUpdateItem').on('click',function (){
+  let idval = $(`#upItemId`).val();
+  searchItem(idval, function (exists) {
+      if (exists) {
+         // Continue with your existing code for updating the items
+         updateItem();
+      } else {
+          clearUpdateTxt();
+          alert("Code doesnot exists. Please choose a existing Code.");
+      }
+  });
 });
 
-// --------------Get all btn event---------------------------
-$("#btnGetAllItem").click(function () {
+function updateItem(){
+      let consent = confirm("Do you really want to update this item.?");
+      if (consent) {
+          let code = $(`#upItemId`).val();
+          let description = $(`#upItemdesc`).val();
+          let unitPrice = $(`#upUnitPrice`).val();
+          let qtyOnHand = $(`#upQty`).val();
+
+          const itemupdateObj = {
+              code:code,
+              description:description,
+              unitPrice:unitPrice,
+              qtyOnHand:qtyOnHand
+  };
+
+  const jsonitmupdate = JSON.stringify(itemupdateObj);
+      
+  $.ajax({
+      url: "http://localhost:8080/app/items",
+      method: "PUT",
+      data: jsonitmupdate,
+      contentType: "application/json",
+      success: function (resp, textStatus, jqxhr) {
+          console.log("success: ", resp);
+          console.log("success: ", textStatus);
+          console.log("success: ", jqxhr);
+          getAllItem();
+      },
+      error: function (jqxhr, textStatus, error) {
+          console.log("error: ", jqxhr);
+          console.log("error: ", textStatus);
+          console.log("error: ", error);
+      }
+  })
+      clearUpdateTxt();
+  }
+  
+}
+
+//save the item
+$('#btnSaveItem').on('click', function () {
+  let id = $('#txtItemId').val();
+  searchItem(id, function (exists) {
+      if (exists) {
+          clearItemTxt();
+          alert("Code already exists. Please choose a different Code.");
+      } else {
+          saveItem();
+      }
+  });
+});
+
+function saveItem() {
+  
+  let code = $(`#txtItemId`).val();
+  let description = $(`#txtItemdec`).val();
+  let unitPrice = $(`#txtItemUnitPrice`).val();
+  let qtyOnHand = $(`#txtItemQty`).val();
+
+          const itemObj = {
+              code: code,
+              description: description,
+              unitPrice: unitPrice,
+              qtyOnHand: qtyOnHand
+          };
+
+          const jsonitmObj = JSON.stringify(itemObj);
+
+          $.ajax({
+              url: "http://localhost:8080/app/items",
+              method: "POST",
+              data: jsonitmObj,
+              contentType: "application/json",
+              success: function (resp, textStatus, jqxhr) {
+                  console.log("success: ", resp);
+                  console.log("success: ", textStatus);
+                  console.log("success: ", jqxhr);
+                  getAllItem();
+                  if (jqxhr.status == 201)
+                      alert(jqxhr.responseText);
+                      
+              },
+              error: function (jqxhr, textStatus, error) {
+                  console.log("error: ", jqxhr);
+                  console.log("error: ", textStatus);
+                  console.log("error: ", error);
+              }
+          });
+
+        
+          clearItemTxt();
+
+}
+
+//search for items
+function searchItem(id, callback) {
+  $.ajax({
+      url: "http://localhost:8080/app/items",
+      method: "GET",
+      success: function (resp) {
+          // ID exists
+              for (const item of resp) {
+                  if(item.code == id) {
+                      callback(true);
+                      return;
+                  }
+              }
+              callback(false);
+      },
+      error: function (jqxhr, textStatus, error) {
+          // ID does not exist
+              callback(false);
+              console.log("Error checking ID existence: ", jqxhr, textStatus, error);
+      }
+  });
+}
+
+//get the item
+$('#btnGetAllItem').on('click', function () {
   getAllItem();
 });
 
-// --------------Search customer function---------------------------
-function searchItem(itemCode) {
-  return itemsDB.find(function (item) {
-    return item.code == itemCode;
-  });
-}
+function getAllItem() {
+  $('#Item-body').empty();
 
-// --------------Save Customer function---------------------------
-async function saveItem() {
-  let itemCode = $("#txtItemCode").val();
-  let itemName = $("#txtItemName").val();
-  let itemPrice = $("#txtItemPrice").val();
-  let itemQuantity = $("#txtItemQuantity").val();
+  $.ajax({
+      url : "http://localhost:8080/app/items",
+      method : "GET",
+      success : function (resp) {
+          console.log("Success: ", resp);
+          for (const item of resp) {
+              console.log(item.code);
+              console.log(item.description);
+              console.log(item.unitPrice);
+              console.log(item.qty);
 
+             $(`#Item-body`).append(`<tr>
+                              <td>${item.code}</td>
+                              <td>${item.description}</td>
+                              <td>${item.unitPrice}</td>
+                              <td>${item.qtyOnHand}</td>
+                              <td><button type="button" class="btn btn-primary btn-sm me-2" data-bs-toggle="modal"
+                                      data-bs-target="#update-model">
+                                  Edit
+                              </button>
+                              <button class="btn btn-danger me-3 btn-sm deleteItem">Delete</button></td>
+                 
+                           </tr>`);
+              setEvent();
+          }
 
-    let itemSend = {
-        code: itemCode,
-        description: itemName,
-        qtyOnHand: itemQuantity,
-        unitPrice: itemPrice,
-
-    }
-
-    console.log(itemSend)
-
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(itemSend),
-  };
-
-
-   const response= await fetch("http://localhost:8080/pos_system_backend_war/items", requestOptions);
-    
-  // check customer if exists or not
-  if (response.ok) {
-    clearItemInputFields();
-    getAllItem();
-    alert("Item added!");
-    loadItemCodes();
-  } else {
-    alert("Item exists!");
-    clearItemInputFields();
-  }
-}
-
-// --------------Get all customer function---------------------------
-// function getAllItem() {
-//   $("#tblItem").empty();
-//   $("#modalItemTable").empty();
-
-//   for (let i = 0; i < itemsDB.length; i++) {
-//     let id = itemsDB[i].code;
-//     let name = itemsDB[i].name;
-//     let price = itemsDB[i].price;
-//     let quantity = itemsDB[i].quantity;
-
-//     let row = `<tr>
-//                         <td>${id}</td>
-//                         <td>${name}</td>
-//                         <td>${price}</td>
-//                         <td>${quantity}</td>
-//                     </tr>`;
-
-//     $("#tblItem").append(row);
-//     $("#modalItemTable").append(row);
-
-//     bindTableRowEventsItem();
-//   }
-// }
-async function getAllItem() {
-  $("#tblItem").empty();
-  $("#modalItemTable").empty();
-
-  const response = await fetch("http://localhost:8080/pos_system_backend_war/items");
-  if (response.ok) {
-    const itemsDB = await response.json();
-
-    for (let i = 0; i < itemsDB.length; i++) {
-      let id = itemsDB[i].code;
-      let name = itemsDB[i].description;
-      let price = itemsDB[i].unitPrice;
-      let quantity = itemsDB[i].qtyOnHand;
-
-      let row = `<tr>
-                          <td>${id}</td>
-                          <td>${name}</td>
-                          <td>${price}</td>
-                          <td>${quantity}</td>
-                      </tr>`;
-
-      $("#tblItem").append(row);
-      $("#modalItemTable").append(row);
-
-      bindTableRowEventsItem();
-    }
-  } else {
-    alert("Failed to fetch items!");
-  }
-}
-
-// --------------Bind row to fields function---------------------------
-function bindTableRowEventsItem() {
-  $("#tblItem>tr").click(function () {
-    let code = $(this).children().eq(0).text();
-    let name = $(this).children().eq(1).text();
-    let price = $(this).children().eq(2).text();
-    let quantity = $(this).children().eq(3).text();
-
-    $("#txtItemCode").val(code);
-    $("#txtItemName").val(name);
-    $("#txtItemPrice").val(price);
-    $("#txtItemQuantity").val(quantity);
-
-    $("#btnDeleteItem").prop("disabled", false);
-  });
-}
-
-// --------------Delete btn event---------------------------
-// $("#btnDeleteItem").click(function () {
-//   let code = $("#txtItemCode").val();
-
-//   let confirmation = confirm("Are you want to delete " + code + " ?");
-//   if (confirmation) {
-//     let response = deleteItem(code);
-//     if (response) {
-//       clearItemInputFields();
-//       getAllItem();
-//       alert("Item deleted!.");
-//     } else {
-//       alert("Item not deleted!.");
-//     }
-//   }
-// });
-
-// --------------Delete customer function---------------------------
-
-
-// function deleteItem(code) {
-//   for (let i = 0; i < itemsDB.length; i++) {
-//     if (itemsDB[i].code == code) {
-//       itemsDB.splice(i, 1);
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-async function deleteItem(code) {
-  const requestOptions = {
-    method: 'DELETE',
-    headers: {"Content-Type": "application/json"}
-  };
-
-  const response = await fetch(`http://localhost:8080/pos_system_backend_war/items/${code}`, requestOptions);
-  return response.ok;
-}
-
-
-// --------------Update btn event---------------------------
-$("#btnUpdateItem").click(function () {
-  let code = $("#txtItemCode").val();
-  updateItem(code);
-  clearItemInputFields();
-});
-
-// --------------Update Customer function---------------------------
-// function updateItem(code) {
-//   if (searchItem(code) == undefined) {
-//     alert("No such item. Please check the ID!");
-//   } else {
-//     let confirmation = confirm("Do you really want to update " + code + ".?");
-//     if (confirmation) {
-//       let item = searchItem(code);
-
-//       let name = $("#txtItemName").val();
-//       let price = $("#txtItemPrice").val();
-//       let quantity = $("#txtItemQuantity").val();
-
-//       item.name = name;
-//       item.price = price;
-//       item.quantity = quantity;
-
-//       getAllItem();
-//       alert("Item updated!");
-//     }
-//   }
-// }
-async function updateItem(code) {
-  if (searchItem(code) == undefined) {
-    alert("No such item. Please check the ID!");
-  } else {
-    let confirmation = confirm("Do you really want to update " + code + ".?");
-    if (confirmation) {
-      let item = searchItem(code);
-
-      let name = $("#txtItemName").val();
-      let price = $("#txtItemPrice").val();
-      let quantity = $("#txtItemQuantity").val();
-
-      let itemUpdate = {
-        code: code,
-        description: name,
-        qtyOnHand: quantity,
-        unitPrice: price
-      };
-
-      const requestOptions = {
-        method: 'PUT',
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(itemUpdate)
-      };
-
-      const response = await fetch(`http://localhost:8080/pos_system_backend_war/items/${code}`, requestOptions);
-
-      if (response.ok) {
-        getAllItem();
-        alert("Item updated!");
-      } else {
-        alert("Failed to update item!");
+      },
+      error : function (error) {
+          console.log("error: ", error);
       }
-    }
+  })
+}
+
+let eventsBound2 = false;
+
+//Bind EDIT And Delete events
+function setEvent() {
+
+  if (!eventsBound2) {
+      $('#tblItem').on('click', 'tr', function () {
+          var $row = $(this).closest("tr"),
+              $tds = $row.find("td:nth-child(1)"),
+              $ts = $row.find("td:nth-child(2)"),
+              $tt = $row.find("td:nth-child(3)"),
+              $tf = $row.find("td:nth-child(4)");
+
+              $(`#upItemId`).val($tds.text());
+              $(`#upItemdesc`).val($ts.text());
+              $(`#upUnitPrice`).val($tt.text());
+              $(`#upQty`).val($tf.text());
+      });
+
+      $('#tblItem').on('click', '.deleteItem', function (event) {
+
+          event.stopPropagation(); // Prevent click event from reaching tr elements
+
+          var $row = $(this).closest("tr"),
+              $tds = $row.find("td:nth-child(1)");
+
+          searchItem($tds.text(), function (exists) {
+              if (exists) {
+                  deleteItem($tds.text());
+              } else {
+                  alert("No such item..please check the code");
+              }
+          });
+      });
+
+      eventsBound2 = true;
+
   }
 }
 
+setEvent();
 
-// --------------Clear btn event---------------------------
-$("#btnClearItem").click(function () {
-  clearItemInputFields();
+//delete item
+function deleteItem(code) {
+  $.ajax({
+      url: "http://localhost:8080/app/items?code=" + code,
+      method: "DELETE",
+      success: function (resp, textStatus, jqxhr) {
+          console.log("success: ", resp);
+          console.log("success: ", textStatus);
+          console.log("success: ", jqxhr);
+          getAllItem();
+      },
+      error: function (jqxhr, textStatus, error) {
+          console.log("error: ", jqxhr);
+          console.log("error: ", textStatus);
+          console.log("error: ", error);
+      }
+  })
+}
+
+
+//search for items
+$('#txtSearchItem').on('keyup', function () {
+  let txtitmVal = $('#txtSearchItem').val();
+
+  if (txtitmVal === '') {
+      getAllItem();
+      return;
+  }
+
+  $(`#Item-body`).empty();
+  const itmType = $("#itemSearch").val();
+
+  $.ajax({
+      url: "http://localhost:8080/app/items",
+      method: "GET",
+      success: function (resp) {
+          console.log("Success: ", resp);
+
+          for (const item of resp) {
+              const itmText = (itmType === "Code") ? item.code : item.description;
+
+              if (itmText.includes($("#txtSearchItem").val())) {
+                  const itmRow = `<tr>
+                      <td>${item.code}</td>
+                      <td>${item.description}</td>
+                      <td>${item.unitPrice}</td>
+                      <td>${item.qtyOnHand}</td>
+                      <td>
+                      <button type="button" class="btn btn-primary btn-sm me-2" data-bs-toggle="modal" data-bs-target="#update-model">Edit</button>
+                      <button class="btn btn-danger me-3 btn-sm deleteItem">Delete</button></td>
+                  </tr>`;
+
+                  $("#tblItem > tbody").append($(`#Item-body`).append(itmRow));
+                  setEvent();
+              }
+          }
+      },
+      error: function (error) {
+          console.log("error: ", error);
+      }
+  });
 });
+
+getAllItem();
